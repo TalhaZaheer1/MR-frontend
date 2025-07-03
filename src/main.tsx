56b3f7058cc564485f5ceb486f8da190c5b50c5e
TalhaZaheer1 <tzaheer72@gmail.com>
2025-07-03 13:36:20 +0500

    main changes

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import AuthProvider from "./providers/AuthProvider.tsx";
 import axios from "axios";
 import { MantineProvider } from "@mantine/core";
 
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
